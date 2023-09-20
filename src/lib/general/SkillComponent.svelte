f65d4e1ff0bafa7b4edc842ec517c16ef15e04a4
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-09-20 01:06:21 -0700

    changes

--- src/lib/general/SkillComponent.svelte
+++ src/lib/general/SkillComponent.svelte
@@ -4,7 +4,14 @@
     export let data;
 </script>
 <div
-  class="relative block lg:w-96 sm:w-auto md:w-96 rounded-lg m-4 text-white bg-[#1E2331] shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] ">
+  class="relative 
+          block lg:w-96 
+          sm:w-auto md:w-96 
+          rounded-lg m-4 
+          text-white bg-[#1E2331] 
+          shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]
+          pb-10
+          ">
   <div class="p-10">
     <h5
       class="mb-2 text-2xl font-black leading-tight">
@@ -17,7 +24,7 @@
   <ul class="w-full">
     {#each data as item}
     <li
-      class="w-full border-b-2 border-neutral-100 border-opacity-100 px-6 py-2 dark:border-opacity-5">
+      class="w-full border-b-[1px] border-neutral-100 border-opacity-100 px-10 py-[6px] dark:border-opacity-5">
       {item}
     </li>
     {/each}
