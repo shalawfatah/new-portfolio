1bd9e9782be4fc90748099201605bf39b98ac581
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-09-25 02:09:31 -0700

    changes

---
+++ src/lib/contact/ContactBlock.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let link;
+    export let text;
+    export let target = "_blank"
+</script>
+
+<div class="hover:bg-gray-800 trasnition-all duration-300 icon rounded-md shadow-sm shadow-gray-200 p-4">
+    <a href={link} target={target}>
+        <div class="w-24 mx-auto">
+            <slot />
+        </div>
+        <div>
+            <p>{text}</p>
+        </div>
+    </a>
+</div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+  import ContactBlock from "../../lib/contact/ContactBlock.svelte";
+  import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte';
+  import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+  import MdEmail from 'svelte-icons/md/MdEmail.svelte';
+  import IoIosPin from 'svelte-icons/io/IoIosPin.svelte';
+</script>
 <svelte:head>
 	<title>Software Developer Portfolio: Shalaw Fatah, Contact</title>
   <meta charset="UTF-8">
@@ -13,44 +20,52 @@
         <svg class="h-20 mx-auto  text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
         </svg> 
-        <div class="max-w-screen-lg mx-8">
-            <p class="leading-10 text-center">
-                I'm a software developer. I've built websites, worked on both frontend and backend, and on mobile applications. The language I'm best at is Javascript, but I might try Python for real soon.
+        <div class="max-w-2xl mx-8">
+            <p class="leading-10 first-letter">
+                My name is Shalaw. I'm a software developer. I've built websites, worked on both frontend and backend, and on mobile applications. The language I'm best at is Javascript, but I might try Python for real soon.
             </p>
-                <p class="leading-10 text-center">I love coding and it's becoming a true passion for me.</p>
+                <p class="leading-10 ">I love coding and it's becoming a true passion for me.</p>
             
-            <p class="leading-10 text-center">
+            <p class="leading-10 first-letter ">
                 There is a true potential to build things that could make an impact with coding. However, what's more important is to have a creative mindset to cultivate your passion.
             </p>
-            <p class="leading-10 text-center">
+            <p class="leading-10">
                 My goal is to use my knowledge to build a project that is truly impactful, to help the way advance towards a better world.
-                <p class="leading-10 text-center">However, this is not a given.</p>
-            <p class="leading-10 text-center">
+                <p class="leading-10 ">However, this is not a given.</p>
+            <p class="leading-10 first-letter ">
                 I have to learn every day and work hard to reach a point where the only thing that is remaining is your ability to think of solutions.
             </p>
-            <p class="leading-10 text-center">
+            <p class="leading-10 ">
                 Until that, I'll work with any project and anyone I deem good.
             </p>
 
             <div class="mt-12">
                 <h1 class="text-4xl font-bold text-center my-4 text-white">Contact me</h1>
-                <div class="flex justify-center fill-[#4B5563] my-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" id="contacts"><path fill="none" d="M0 0h48v48H0zm0 0h48v48H0zm0 0h48v48H0z"></path><path d="M40 0H8v4h32V0zM8 48h32v-4H8v4zM40 8H8c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V12c0-2.21-1.79-4-4-4zm-16 5.5c2.48 0 4.5 2.02 4.5 4.5a4.5 4.5 0 0 1-9 0c0-2.48 2.02-4.5 4.5-4.5zM34 34H14v-3c0-3.33 6.67-5 10-5s10 1.67 10 5v3z"></path></svg>            </div>
                 <div class="my-4 flex justify-center">
-                    <div class="text-center">
-                        <p><span class="font-bold">Address:</span> Somewhere in Canada</p>
-                        <p>
-                            <a href="mailto:shalaw.fatah@gmail.com?subject=Contact%20email%20from%20your%20website"><span class="font-bold">Email: </span> shalaw.fatah@gmail.com</a>
-                        </p>
-                        <p>
-                            <a target="_blank" href="https://linkedin.com/in/shalawfatah"><span class="font-bold">LinkedIn: </span>@shalawfatah</a>
-                        </p>
-                        <p>
-                            <a target="_blank" href="https://github.com/shalawfatah"><span class="font-bold">Github: </span> @shalawfatah</a>
-                        </p>
+                    <div class="flex flex-wrap justify-center gap-4">
+                        <ContactBlock target="_self" link="#" text="@Canada">
+                            <IoIosPin />
+                        </ContactBlock>
+                        <ContactBlock link="mailto:shalaw.fatah@gmail.com" text="@shalaw.fatah">
+                            <MdEmail />
+                        </ContactBlock>
+                        <ContactBlock link="https://linkedin.com/in/shalawfatah" text="@shalawfatah">
+                            <FaLinkedin />
+                        </ContactBlock>
+                        <ContactBlock link="https://github.com/shalawfatah" text="@shalawfatah">
+                            <DiGithubBadge />
+                        </ContactBlock>
                     </div>
                 </div>
             </div>
             </div>
     </div>
 </div>
+
+<style>
+    .first-letter::first-letter {
+        font-size: 40px;
+        font-weight: 900; /* This is equivalent to Tailwind's 'font-black' */
+        color: #34509C;
+        }
+</style>
