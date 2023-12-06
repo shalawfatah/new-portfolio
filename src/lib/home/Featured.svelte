f8d517eb101bad8d0e065542d5e2c4f1f6dbbb43
Shalaw Fatah <shalaw.fatah@gmail.com>
2023-12-05 17:21:50 -0800

    changes

--- src/lib/home/Featured.svelte
+++ src/lib/home/Featured.svelte
@@ -28,9 +28,9 @@
     }
 </script>
 
-<div class="relative border-4 border-gray-100 rounded-lg">
-    <Img class="opacity-60 hover:opacity-100 duration-300 w-96 h-60 shadow-sm shadow-gray-300" {src} alt={title} />
-    <h5 class="mb-1 text-xl font-bold text-white my-4 text-center">{title}</h5>
+<div class="relative border-4 border-gray-100 rounded-lg w-96 h-60 mb-16">
+    <Img class="opacity-60 hover:opacity-100 duration-300 w-full h-full shadow-sm shadow-gray-300" {src} alt={title} />
+    <h5 class="mb-1 text-xl font-bold text-white my-4 text-center p-4">{title}</h5>
     <div class="flex justify-center items-center">
         <div class="absolute bottom-16 flex mt-4 space-x-3 md:mt-6">
             <a target="_blank" href={live} class="shadow-md shadow-black inline-flex items-center px-8 py-2 text-sm font-black text-center text-gray-900 bg-[#FF7B7B] rounded-lg hover:bg-blue-800 hover:text-white focus:ring-4 duration-300 focus:outline-none focus:ring-blue-300 ">Live</a>
--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
 import { supabase } from '../supabase/supabase.js';
 
 export async function load() {
-    const {data, error} = await supabase.from('projects').select().in('id', ['58043cc1-6401-4c0c-ada7-47bef9a894f3', '252191da-67b4-4dcb-ab50-193f3c75b07f', '89165d4e-22c6-4955-b1a3-3688c8887ffd']).order('created_at', {ascending: false})
+    const {data, error} = await supabase.from('projects').select().in('id', ['58043cc1-6401-4c0c-ada7-47bef9a894f3', '252191da-67b4-4dcb-ab50-193f3c75b07f', '89165d4e-22c6-4955-b1a3-3688c8887ffd', '7712cb5c-45be-4c13-8816-c34e02d69e57']).order('created_at', {ascending: false})
     if(error) {
         return error;
     }
